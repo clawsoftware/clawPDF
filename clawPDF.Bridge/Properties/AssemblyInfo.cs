d66e67959b5febc6ba4c112f79f7df95072b6aff
Andrew Hess <45692132+hessandrew@users.noreply.github.com>
2023-03-20 01:15:16 +0100

    0.8.7

--- clawPDF.Bridge/Properties/AssemblyInfo.cs
+++ clawPDF.Bridge/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.6.0")]
-[assembly: AssemblyFileVersion("0.8.6.0")]
+[assembly: AssemblyVersion("0.8.7.0")]
+[assembly: AssemblyFileVersion("0.8.7.0")]
--- clawPDF/Views/PrintJobWindow.xaml.cs
+++ clawPDF/Views/PrintJobWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 using clawSoft.clawPDF.Core.Settings;
+using clawSoft.clawPDF.Core.Settings.Enums;
 using clawSoft.clawPDF.Helper;
 using clawSoft.clawPDF.Shared.Helper;
+using clawSoft.clawPDF.Shared.ViewModels;
+using clawSoft.clawPDF.Shared.Views;
 using clawSoft.clawPDF.ViewModels;
 using clawSoft.clawPDF.WindowsApi;
 
@@ -10,15 +14,18 @@ namespace clawSoft.clawPDF.Views
     internal partial class PrintJobWindow
     {
         private clawPDFSettings _settings = SettingsHelper.Settings;
+        private PrintJobViewModel vm;
 
         public PrintJobWindow()
         {
             InitializeComponent();
+            vm = new PrintJobViewModel();
+            DataContext = vm;
         }
 
         private void SettingsButton_OnClick(object sender, RoutedEventArgs e)
         {
-            var vm = (PrintJobViewModel)DataContext;
+            //var vm = (PrintJobViewModel)DataContext;
 
             TopMostHelper.UndoTopMostWindow(this);
             _settings.ApplicationSettings.LastUsedProfileGuid = vm.SelectedProfile.Guid;
@@ -54,6 +61,63 @@ namespace clawSoft.clawPDF.Views
         private void OnDrop(object sender, DragEventArgs e)
         {
             DragAndDropHelper.Drop(e);
+        }
+
+        private void SecurityPasswordsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //var vm = (PrintJobViewModel)DataContext;
+
+            var askUserPassword = vm.SelectedProfile.PdfSettings.Security.RequireUserPassword;
+
+            var pwWindow = new EncryptionPasswordsWindow(EncryptionPasswordMiddleButton.Remove, true, askUserPassword);
+            pwWindow.OwnerPassword = vm.SelectedProfile.PdfSettings.Security.OwnerPassword;
+            pwWindow.UserPassword = vm.SelectedProfile.PdfSettings.Security.UserPassword;
+
+            pwWindow.ShowDialogTopMost();
+
+            if (pwWindow.Response == EncryptionPasswordResponse.OK)
+            {
+                vm.SelectedProfile.PdfSettings.Security.OwnerPassword = pwWindow.OwnerPassword;
+                vm.SelectedProfile.PdfSettings.Security.UserPassword = pwWindow.UserPassword;
+            }
+            else if (pwWindow.Response == EncryptionPasswordResponse.Remove)
+            {
+                vm.SelectedProfile.PdfSettings.Security.UserPassword = "";
+                vm.SelectedProfile.PdfSettings.Security.OwnerPassword = "";
+            }
+        }
+
+        private void profileChange(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            SecurityCheckBox.IsChecked = vm.SelectedProfile.PdfSettings.Security.Enabled;
+            UserPasswordCheckBox.IsChecked = vm.SelectedProfile.PdfSettings.Security.RequireUserPassword;
+            OCRLanguage.Text = vm.SelectedProfile.OCRSettings.OCRLanguage;
+        }
+
+        private void SecurityCheckBoxChecked(object sender, RoutedEventArgs e)
+        {
+            vm.SelectedProfile.PdfSettings.Security.Enabled = true;
+            vm.SelectedProfile.PdfSettings.Security.EncryptionLevel = EncryptionLevel.Aes128Bit;
+        }
+
+        private void SecurityCheckBoxUnchecked(object sender, RoutedEventArgs e)
+        {
+            vm.SelectedProfile.PdfSettings.Security.Enabled = false;
+        }
+
+        private void UserPasswordCheckBoxChecked(object sender, RoutedEventArgs e)
+        {
+            vm.SelectedProfile.PdfSettings.Security.RequireUserPassword = true;
+        }
+
+        private void UserPasswordCheckBoxUnchecked(object sender, RoutedEventArgs e)
+        {
+            vm.SelectedProfile.PdfSettings.Security.RequireUserPassword = true;
+        }
+
+        private void OCRLanguageChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            vm.SelectedProfile.OCRSettings.OCRLanguage = OCRLanguage.Text;
         }
     }
 }
